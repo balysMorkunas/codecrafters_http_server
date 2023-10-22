cfdd18ba2e6b5540a17351fd9efb644396a841d9
balys_morkunas <balys.morkunas@gmail.com>
2023-10-22 20:44:08 +0300

    respond with echo content

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,20 @@ fn main() {
                 let mut buffer = [0; 512];
                 let _ = stream.read(&mut buffer);
                 let request = String::from_utf8_lossy(&buffer[..]).to_string();
+                println!("{}", request);
 
                 // ex: GET /index.html HTTP/1.1
                 let header: Vec<&str> = request.split_once("\r\n").unwrap().0.split(" ").collect();
 
-                let path = header[1];
-                match path {
-                    "/" => respond(stream, "HTTP/1.1 200 OK\r\n\r\n"),
+                let path: Vec<&str> = header[1].split("/").collect();
+
+                match path[1] {
+                    "echo" => {
+                        let response_string = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", path[2].len(), path[2]);
+
+                        respond(stream, &response_string)
+                    }
+                    "" => respond(stream, "HTTP/1.1 200 OK\r\n\r\n"),
                     _ => respond(stream, "HTTP/1.1 404 Not Found\r\n\r\n"),
                 };
             }
